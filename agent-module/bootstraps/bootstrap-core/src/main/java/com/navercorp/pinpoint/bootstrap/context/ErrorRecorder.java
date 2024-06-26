7f42a1b1b05393eba915a7bfdaac00c016a5b42f
Ohjun Kwon <dev@kojandy.com>
2024-06-26 19:41:40 +0900

    feat: add ErrorCategory

--- agent-module/bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/context/ErrorRecorder.java
+++ agent-module/bootstraps/bootstrap-core/src/main/java/com/navercorp/pinpoint/bootstrap/context/ErrorRecorder.java
@@ -1,5 +1,12 @@
 package com.navercorp.pinpoint.bootstrap.context;
 
+import com.navercorp.pinpoint.common.trace.ErrorType;
+
 public interface ErrorRecorder {
-    void recordError();
+    void recordError(ErrorType errorType);
+
+    @Deprecated
+    default void recordError() {
+        recordError(ErrorType.UNKNOWN);
+    }
 }
--- agent-module/profiler/src/main/java/com/navercorp/pinpoint/profiler/context/recorder/TraceRootSpanRecorder.java
+++ agent-module/profiler/src/main/java/com/navercorp/pinpoint/profiler/context/recorder/TraceRootSpanRecorder.java
@@ -20,6 +20,7 @@ import com.navercorp.pinpoint.bootstrap.context.ErrorRecorder;
 import com.navercorp.pinpoint.bootstrap.context.MethodDescriptor;
 import com.navercorp.pinpoint.bootstrap.context.SpanRecorder;
 import com.navercorp.pinpoint.common.trace.AnnotationKey;
+import com.navercorp.pinpoint.common.trace.ErrorType;
 import com.navercorp.pinpoint.common.trace.LoggingInfo;
 import com.navercorp.pinpoint.common.trace.ServiceType;
 import com.navercorp.pinpoint.common.util.DataType;
@@ -64,14 +65,14 @@ public class TraceRootSpanRecorder implements SpanRecorder {
     }
 
     @Override
-    public void recordError() {
-        errorRecorder.recordError();
+    public void recordError(ErrorType errorType) {
+        errorRecorder.recordError(errorType);
     }
 
     @Override
     public void recordException(boolean markError, Throwable throwable) {
         if (markError) {
-            recordError();
+            recordError(ErrorType.EXCEPTION);
         }
     }
 
